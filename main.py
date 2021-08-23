155dd2917455b7b3903c31d26e68b715c12fe28c
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2021-08-23 18:51:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import eel
-import module
+import Static.module as module
+import tkinter as tk
+from tkinter import filedialog
 
 
+@eel.expose
+def getFolderPath():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askdirectory()
+    root.destroy()
+    print(file_path)
+    return file_path
 
-
-
+@eel.expose
+def download(url,path):
+    installer = module.installer(path)
+    installer.downloader(url)
 
 
 eel.init("GUI")
-eel.start("main.html")
+eel.start("main.html",port=8081)
