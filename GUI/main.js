2ca18c10d0caaf5d7d2a10ac6bd490424e044494
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2021-10-11 21:19:43 +0800

    nothing

--- GUI/main.js
+++ GUI/main.js
@@ -1,22 +1,28 @@
 async function callGetFolderPath(){
+    console.log("callGetFolderPath")
     path = await eel.getFolderPath()();
-    document.getElementById("path").value = path;
+    if(path !== ""){
+        document.getElementById("path").value = path;
+    }
 }
 
 async function callGetDefaultPath(){
+    console.log("callGetDefaultPath")
     path = await eel.getDefaultPath()();
     $("#path").val(path);
 }
 
 async function callDownload(){
+    console.log("callDownload") 
     resetProgressBar();
     url = document.getElementById("url").value
     path = document.getElementById("path").value;
     await eel.download(url,path)();
 }
 
-function reset(){
-    document.getElementById("path").value = "";
+async function reset(){
+    console.log("reset") 
+    document.getElementById("path").value = await eel.getDefaultPath()();
     document.getElementById("url").value = "";
     resetProgressBar();
 }
@@ -24,14 +30,17 @@ function reset(){
 eel.expose(changeProgressBar);
 
 function changeProgressBar(installedSize){
+    console.log("changeProgressBar") 
     $(".progress-bar").css("width", installedSize + "%").text(installedSize + " %");
 }
 
 async function callOpenOtherPage(){
+    console.log("callOpenOtherPage") 
     await eel.openOtherPage()();
 }
 
 function resetProgressBar(){
+    console.log("resetProgressBar") 
     $(".progress-bar").css("width", 0 + "%").text(0 + " %");
 }
 
--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import Static.module as module
 import tkinter as tk
 from tkinter import filedialog
 import json
+import sys
+sys.coinit_flags = 2  # COINIT_APARTMENTTHREADED
+import pywinauto
+import pyglet
 
 @eel.expose
 def getFolderPath():
+    print("getFolderPath")
     root = tk.Tk()
     root.withdraw()
-    path = filedialog.askdirectory()
+    # https://github.com/pywinauto/pywinauto/issues/517
+    path = filedialog.askdirectory()# 很常死在這行
     print(path)
     root.destroy()
-    if not path:
-        path = getDefaultPath()
     return path
 
 @eel.expose
 def download(url,path):
+    print("download")
     print(url)
     installer = module.installer(path)
     installedSize = 0
@@ -24,10 +29,12 @@ def download(url,path):
    
 @eel.expose
 def openOtherPage():
+    print("openOtherPage")
     eel.show("setting.html")
 
 @eel.expose
 def getDefaultPath():
+    print("getDefaultPath")
     with open("Static/setting.json","r",encoding="utf8") as jfile:
         jdata = json.load(jfile)
     path = jdata["downloadPath"]
@@ -35,6 +42,7 @@ def getDefaultPath():
 
 @eel.expose
 def savePath(path):
+    print("savePath")
     with open("Static/setting.json","r",encoding="utf8") as jfile:
         jdata = json.load(jfile)
     jdata["downloadPath"] = path
